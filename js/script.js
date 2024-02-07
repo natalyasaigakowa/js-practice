0074858de1875b12aae55869ac9dec98a3debaf9
Natalya <natalisaig@yandex.ru>
2024-02-07 19:08:09 +0600

    second

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-"use script";
+"use strict";
 
-// alert("Hello");
-// const result = confirm("Are you here?");
-// console.log(result);
 
-const answer = +prompt("Вам есть 18?", "18");
-console.log(typeof(answer));
