4ee771dc2a88786bfea60ba13e9b7f95ee4d59b8
Natalya <natalisaig@yandex.ru>
2024-02-07 18:57:37 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use script";
+
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = +prompt("Вам есть 18?", "18");
+console.log(typeof(answer));
